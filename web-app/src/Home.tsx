59be704b7fc26fec578ee24101a72d835593dcfe
iankietzman-voeracity <ian+voeracity@tallgrassschool.com>
2024-11-25 10:15:05 -0600

    adds cheeky homepage

--- web-app/src/Home.tsx
+++ web-app/src/Home.tsx
@@ -1,8 +1,13 @@
+import eleven from './assets/11.gif'
+
 export default function Home() {
   return (
-    <div>
+    <div className="text-center">
       <h2 className="text-xl p-2">Home</h2>
-      <p>Welcome to 11x Genomics</p>
+      <h3 className="text-l p-2">Welcome to 11x Genomics</h3>
+      <p>Marty DiBerg (as played by Rob Reiner): Why don't you just make ten <s>louder</s> better and make ten be the top <s>genomics</s> and make that a little <s>louder</s> better? </p>
+      <p>Me:</p>
+      <img src={eleven} className="mx-auto" alt="These go to eleven." />
     </div>
   );
 }
